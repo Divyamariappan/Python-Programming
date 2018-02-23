4d28895b6bd0cdb6bcda72cea6ae4c9e95e3012e
Divyamariappan <36722813+Divyamariappan@users.noreply.github.com>
2018-02-23 10:41:58 +0530

    Create vowel or consonant.py

---
+++ Beginner level/vowel or consonant.py
@@ -0,0 +1,7 @@
+a=str(input())
+if((a=='a')or(a=='e')or(a=='i')or(a=='o')or(a=='u')or(a=='A')or(a=='E')or(a=='I')or(a=='O')or(a=='U')):
+    print("Vowel letter")
+elif(int(a)):
+    print("not valid")
+else:
+    print("Consonant")
