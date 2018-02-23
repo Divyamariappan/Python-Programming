335e0241343afeab7fee2184a526ebcdb305983f
Divyamariappan <36722813+Divyamariappan@users.noreply.github.com>
2018-02-23 09:40:27 +0530

    Create positive or negative or zero.py

---
+++ Beginner level/positive or negative or zero.py
@@ -0,0 +1,10 @@
+a=input()
+if((int(a)>=1) and (int(a)<=100000)):
+  print("Positive")
+elif(int(a)==0):
+  print("Zero")
+elif(int(a)<0):
+  print("Negative")
+else:
+  print("Positive but Exceed")
+
