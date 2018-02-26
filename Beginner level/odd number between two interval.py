085978d5cf5b2d9ee1e3011c66305f7415a6a00d
Divyamariappan <36722813+Divyamariappan@users.noreply.github.com>
2018-02-26 11:13:22 +0530

    Update odd number between two interval.py

--- Beginner level/odd number between two interval.py
+++ Beginner level/odd number between two interval.py
@@ -2,6 +2,8 @@ N=int(input())
 Q=int(input())
 if((N<=100000)or(Q<=100000) or(N>100000)or (Q>100000)):
     for i in range(N+1,Q):
-      if(i%2!=0)
+      if(i%2!=0):
         print(i,end=' ')
+      else:
+        print("no number")
    
