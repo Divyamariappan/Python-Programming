d882ec258442fb2ac338625771081c46c1f2e789
Divyamariappan <36722813+Divyamariappan@users.noreply.github.com>
2018-02-26 10:14:00 +0530

    Update prime number.py

--- Beginner level/prime number.py
+++ Beginner level/prime number.py
@@ -6,6 +6,6 @@ for i in range(2,a):
       flag=1
       break
 if(flag==0):
-   print("{0} is a prime number".format(a))
+   print("yes")
 else:
-  print("{0} is not a prime number".format(a))
+  print("no")
